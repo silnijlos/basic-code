6a785338662b238820501381e216d440016a4590
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2017-01-17 20:28:20 -0800

    Add files via upload

--- tree.py
+++ tree.py
@@ -7,7 +7,6 @@
 # (with value 100) and it's left child (with value 50), and you're adding a new node with value 150, then no `depth`
 # will change. If in the same initial setup value 10 is added, then `depth` for both 50 and 100 will increase by 1.
 
-
 class Node:
     def __init__(self, value, parent=None, depth=0):
         self.value = value
@@ -37,33 +36,68 @@ class BST:
             self.firstNode = None
 
     def is_leaf(self, node):
-        return not bool(node.leftNode or node.rightNode)
+        if node:
+            return not bool(node.leftNode or node.rightNode)
+        if node==None:
+            return True
+
+    def correct_depth(self, node):
+        if not (node.rightNode and node.leftNode):
+            return 0
+        rd = 0
+        if node.rightNode:
+            rd = self.correct_depth(node.rightNode)
+            node.rightNode.depth = rd
+        ld = 0
+        if node.leftNode:
+            ld = self.correct_depth(node.leftNode)
+            node.leftNode.depth = ld
+        d = max([rd, ld]) + 1
+        node.depth = d
+        print(d)
+        return d
+
+    def get_depth(self, node):
+        if not (node.rightNode and node.leftNode):
+            return 0
+        rd = 0
+        if node.rightNode:
+            rd = self.get_depth(node.rightNode)
+        ld = 0
+        if node.leftNode:
+            ld = self.get_depth(node.leftNode)
+        d = max([rd, ld])+1
+        return d
 
     def add(self, value):
         cur = self.firstNode
         while not self.is_leaf(cur):
             if value > cur.value:
                 if cur.rightNode:
-                    cur.depth += 1
                     cur = cur.rightNode
                     continue
                 break
             elif value < cur.value:
                 if cur.leftNode:
-                    cur.depth += 1
                     cur = cur.leftNode
                     continue
                 break
-        cur.depth += 1
         if value > cur.value:
             new = Node(value, cur, 0)
             cur.rightNode = new
         else:
             new = Node(value, cur, 0)
             cur.leftNode = new
+        cur = new
+        while cur.parent:
+            cur.depth = self.get_depth(cur)
+            cur = cur.parent
+        cur.depth = self.get_depth(cur)
+
 
     def print_inorder(self):
         return self.recursive_printing()
+
 
     def recursive_printing(self, node=None, output=''):
         if not node:
@@ -76,7 +110,6 @@ class BST:
         return output.strip()
 
 t = BST(100)
-t.add(50)
-t.add(200)
-t.add(10)
+for x in [50, 150, 45, 55, 120, 170]:
+    t.add(x)
 print(t.print_inorder())
